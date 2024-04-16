4afa82e5a629ba6f69f5715ed0a4f4000419db3c
Wambura <87015626+Wambura001@users.noreply.github.com>
2024-04-16 12:09:08 +0900

    Add files via upload

---
+++ Calculator.cpp
@@ -0,0 +1,46 @@
+// Calculator.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    char oper;
+    float num1, num2;
+    cout << "Enter an operator (+, -, *, /)";
+    cin >> oper;
+    cout << "Enter two numbers: " << endl;
+    cin >> num1 >> num2;
+
+    switch (oper) {
+    case '+':
+        cout << num1 << "+" << num2 << " = " << num1 + num2;
+        break;
+    case '-':
+        cout << num1 << "-" << num2 << " = " << num1 - num2;
+        break;
+    case '*':
+        cout << num1 << "*" << num2 << " = " << num1 * num2;
+        break;
+    case '/':
+        cout << num1 << "/" << num2 << " = " << num1 / num2;
+        break;
+    default:
+        //operator doesn't match
+        cout << "Error! The operator does not match";
+            break;
+    }
+    return 0;
+}
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
